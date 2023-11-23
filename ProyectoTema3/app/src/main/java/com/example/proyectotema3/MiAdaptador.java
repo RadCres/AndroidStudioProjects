dfc8654c61889a3fb172b625d074ab99e9f45fe0
Raúl <raulduranc@hotmail.com>
2023-11-23 23:48:43 +0100

    Update MiAdaptador.java

--- ProyectoTema3/app/src/main/java/com/example/proyectotema3/MiAdaptador.java
+++ ProyectoTema3/app/src/main/java/com/example/proyectotema3/MiAdaptador.java
@@ -1,12 +1,42 @@
 package com.example.proyectotema3;
 
 
+import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class MiAdaptador extends MiAdaptador.ViewHolder<MiAdaptador.ViewHolder> {
+public class MiAdaptador extends RecyclerView.Adapter<MiAdaptador.ViewHolder> {
+
     private List<String> mData;
     private LayoutInflater mInflater;
-    private ItemClickListener itemClickListener;
+    private AdapterView.OnItemClickListener itemClickListener;
+
+    MiAdaptador(Context Contexto, List<String> data) {
+        this.mData = LayoutInflater.from(contexto);
+        this.mInflater = data;
+    }
+
+
+    @NonNull
+    @Override
+    public MiAdaptador.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MiAdaptador.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
 }
+

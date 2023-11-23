8c2d86d1f1ff792e462abb5a5e307b96fb94efdc
Raúl <raulduranc@hotmail.com>
2023-11-23 20:55:44 +0100

    v.2.asignaturas

--- ProyectoTema3/app/src/main/java/com/example/proyectotema3/MainActivity2.java
+++ ProyectoTema3/app/src/main/java/com/example/proyectotema3/MainActivity2.java
@@ -1,6 +1,5 @@
 package com.example.proyectotema3;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
@@ -8,16 +7,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
-    private ImageButton botonESO;
-    private ImageButton botonFP;
-    private ImageButton botonBachillerato;
+    private static final int MENU_ESO = 1;
+    private static final int MENU_BACHILLERATO = 2;
+
+    private TextView ESO;
+    private TextView bachillerato;
+    private TextView fp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,60 +26,93 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
 
-        botonFP = findViewById(R.id.imageButtonFP);
-        botonESO = findViewById(R.id.imageButtonEso);
-        botonBachillerato = findViewById(R.id.imageButtonBachillerato);
-        registerForContextMenu(botonESO);
+        ESO = findViewById(R.id.textViewESO);
+        bachillerato = findViewById(R.id.textViewBachillerato);
+        fp = findViewById(R.id.textViewFP);
+        registerForContextMenu(ESO);
+        registerForContextMenu(bachillerato);
+        registerForContextMenu(fp);
 
-        botonFP.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity2.this, "Botón pulsado", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-        botonESO.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v2) {
-                Toast.makeText(MainActivity2.this, "Botón pulsado", Toast.LENGTH_SHORT).show();
-
-            }
-        });
     }
 
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menucontextualeso_main2, menu);
+        if (v.getId() == R.id.textViewESO) {
+            // Si el botón ESO está pulsado, inflar el menú contextual de ESO
+            getMenuInflater().inflate(R.menu.menucontextualeso_main2, menu);
+        } else if (v.getId() == R.id.textViewBachillerato) {
+            // Si el botón Bachillerato está pulsado, inflar el menú contextual de Bachillerato
+            getMenuInflater().inflate(R.menu.menucontextualbachillerato_main2, menu);
+        } else {
+            getMenuInflater().inflate(R.menu.menucontextualfp_main2, menu);
+        }
     }
+
 
     public boolean onContextItemSelected(MenuItem item) {
         String opcion = "";
         switch (item.getItemId()) {
             case R.id.itemPrimero:
                 opcion = "PrimeroESO";
-                pulsarOpcionDelMenu(opcion);
-
+                pulsarOpcionDelMenuESO(opcion);
                 return true;
             case R.id.itemSegundo:
                 opcion = "SegundoESO";
-                pulsarOpcionDelMenu(opcion);
+                pulsarOpcionDelMenuESO(opcion);
                 return true;
             case R.id.itemTercero:
                 opcion = "TerceroESO";
-                pulsarOpcionDelMenu(opcion);
+                pulsarOpcionDelMenuESO(opcion);
                 return true;
-
             case R.id.itemCuarto:
                 opcion = "CuartoESO";
-                pulsarOpcionDelMenu(opcion);
+                pulsarOpcionDelMenuESO(opcion);
                 return true;
-
+            case R.id.opcionPrimeroBachillerato:
+                opcion = "PrimeroBachilleraro";
+                pulsarOpcionDelMenuBachillerato(opcion);
+                return true;
+            case R.id.opcionSegundoBachillerato:
+                opcion = "SegundoBachilleraro";
+                pulsarOpcionDelMenuBachillerato(opcion);
+                return true;
+            case R.id.FPopcion1SA:
+                opcion = "1SA";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
+            case R.id.FPopcion2SA:
+                opcion = "2SA";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
+            case R.id.FPopcion1GA:
+                opcion = "1GA";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
+            case R.id.FPopcion2GA:
+                opcion = "2GA";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
+            case R.id.FPopcion1AF:
+                opcion = "1AF";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
+            case R.id.FPopcion2AF:
+                opcion = "2AF";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
+            case R.id.FPopcion1DAM:
+                opcion = "1DAM";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
+            case R.id.FPopcion2DAM:
+                opcion = "2DAM";
+                pulsarOpcionDelMenuFP(opcion);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void pulsarOpcionDelMenu(String opcion) {
+    private void pulsarOpcionDelMenuESO(String opcion) {
         if (opcion.equalsIgnoreCase("PrimeroESO")) {
             String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=740:1eso&catid=11&Itemid=213067";
             try {
@@ -106,8 +140,107 @@ public class MainActivity2 extends AppCompatActivity {
             } catch (ActivityNotFoundException e) {
                 Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
             }
-        } else {
+        } else if (opcion.equalsIgnoreCase("CuartoESO")) {
             String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=744:4eso&catid=11&Itemid=213072";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void pulsarOpcionDelMenuBachillerato(String opcion) {
+        if (opcion.equalsIgnoreCase("PrimeroBachilleraro")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=740:1eso&catid=11&Itemid=213067";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("SegundoBachilleraro")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=751:2-bach&catid=11&Itemid=213469";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
+    private void pulsarOpcionDelMenuFP(String opcion) {
+        if (opcion.equalsIgnoreCase("1SA")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=757:1fpb&catid=154&Itemid=213080";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("2SA")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=758:2fpb&catid=154&Itemid=213081";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("1GA")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=759:ga1&catid=154&Itemid=213082";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("2GA")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=760:ga2&catid=11&Itemid=213083";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("1AF")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=761:1af&catid=154&Itemid=213094";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("2AF")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=763:2af&catid=154&Itemid=213084";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("1DAM")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=764:1dam&catid=154&Itemid=213085";
+            try {
+                // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
+                Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intentoUrl); // Asegúrate de iniciar la actividad
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(getApplicationContext(), "No se pudo abrir la página web", Toast.LENGTH_SHORT).show();
+            }
+        } else if (opcion.equalsIgnoreCase("2DAM")) {
+            String url = "https://iesarroyoharnina.educarex.es/index.php?option=com_content&view=article&id=765:2dam&catid=154&Itemid=213086";
             try {
                 // Utiliza ACTION_VIEW en lugar de ACTION_WEB_SEARCH
                 Intent intentoUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
---
+++ ProyectoTema3/app/src/main/java/com/example/proyectotema3/MiAdaptador.java
@@ -0,0 +1,12 @@
+package com.example.proyectotema3;
+
+
+import android.view.LayoutInflater;
+
+import java.util.List;
+
+public class MiAdaptador extends MiAdaptador.ViewHolder<MiAdaptador.ViewHolder> {
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener itemClickListener;
+}

53895bc855dad4d244eedc779ff7eee57fd1a734
Raúl <raulduranc@hotmail.com>
2023-12-15 00:33:25 +0100

    finiquitando

--- ProyectoTema3/app/src/main/java/com/example/proyectotema3/MainActivity4.java
+++ ProyectoTema3/app/src/main/java/com/example/proyectotema3/MainActivity4.java
@@ -3,27 +3,25 @@ package com.example.proyectotema3;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Calendar;
+
 
 public class MainActivity4 extends AppCompatActivity {
     private CalendarView calendario;
 
-
-    private String date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,45 +29,78 @@ public class MainActivity4 extends AppCompatActivity {
         setContentView(R.layout.activity_main4);
 
         calendario = findViewById(R.id.MiCalendario);
-
-
         getCalendario();
-
-
     }
 
 
-    public void dialogoEditText(final String date) {
-        // Crea un EditText
+    private void dialogoEditText(final String date) {
         final EditText lista = new EditText(this);
 
-        // Lee el contenido del archivo y establece el texto en el EditText
         String contenidoGuardado = leerContenidoGuardado(date);
         lista.setText(contenidoGuardado);
 
-        // Crea un AlertDialog
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setView(lista);
         alertDialogBuilder.setTitle("Notas");
+
         alertDialogBuilder.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                // Acción al hacer clic en el botón Guardar
                 guardar(date, lista.getText().toString());
+
             }
         });
-        alertDialogBuilder.setNegativeButton("Salir", new DialogInterface.OnClickListener() {
+
+        alertDialogBuilder.setNegativeButton("Borrar", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                // Acción al hacer clic en el botón Salir
+                borrarNotas(date);
+            }
+        });
+
+        alertDialogBuilder.setNeutralButton("Salir", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
             }
         });
+
         alertDialogBuilder.setCancelable(false);
 
-        // Muestra el AlertDialog
         AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.show();
+    }
+
+    private void guardar(String date, String contenido) {
+        try {
+            if (!TextUtils.isEmpty(contenido)) {
+                OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput(date, Context.MODE_PRIVATE));
+                archivo.write(contenido);
+                archivo.flush();
+                archivo.close();
+                Toast.makeText(MainActivity4.this, "Nota guardada con éxito", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity4.this, "Guardado sin cambios", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Throwable e) {
+            Toast.makeText(getApplicationContext(),
+                    "Exception: " + e.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void borrarNotas(String date) {
+        try {
+            // Verificar si el archivo existe antes de intentar borrar
+            File file = getFileStreamPath(date);
+            if (file.exists()) {
+                deleteFile(date);
+                Toast.makeText(MainActivity4.this, "Notas borradas", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity4.this, "No hay notas que borrar", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(MainActivity4.this, "Error al borrar las notas", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -79,8 +110,10 @@ public class MainActivity4 extends AppCompatActivity {
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = obtenerFechaComoTexto(year, month, dayOfMonth);
                 dialogoEditText(date);
+
             }
         });
+
     }
 
     private String obtenerFechaComoTexto(int year, int month, int dayOfMonth) {
@@ -106,23 +139,6 @@ public class MainActivity4 extends AppCompatActivity {
         } catch (Exception e) {
             // Manejar la excepción según tus necesidades
             return "";
-        }
-    }
-
-
-    private void guardar(String date, String contenido) {
-        try {
-            if (!TextUtils.isEmpty(contenido)) {
-                OutputStreamWriter archivo = new OutputStreamWriter(openFileOutput(date, Context.MODE_PRIVATE));
-                archivo.write(contenido);
-                archivo.flush();
-                archivo.close();
-            } else {
-                Toast.makeText(MainActivity4.this, "Nueva nota guardada", Toast.LENGTH_SHORT).show();
-            }
-        } catch (Throwable e) {
-            Toast.makeText(getApplicationContext(),
-                    "Exception: " + e.toString(), Toast.LENGTH_SHORT).show();
         }
     }
 

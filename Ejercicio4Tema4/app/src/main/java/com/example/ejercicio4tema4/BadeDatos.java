8dd7c666578277eb8119d7e43f40f7a6be769262
RadCres <raulduranc@hotmail.com>
2024-02-01 14:05:13 +0100

    Empezando Ejercicio4

---
+++ Ejercicio4Tema4/app/src/main/java/com/example/ejercicio4tema4/BadeDatos.java
@@ -0,0 +1,49 @@
+package com.example.ejercicio4tema4;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+
+public class BadeDatos extends SQLiteOpenHelper {
+    Context contexto;
+
+    public BadeDatos(Context context) {
+        super(context,"BD.db", null, 2);
+        contexto = context;
+    }
+
+
+    @Override
+    //onCreate se ejecuta cuando se CREA la BD
+    public void onCreate(SQLiteDatabase db) {
+        try {
+            //Creamos la tabla
+            db.execSQL("CREATE TABLE articulo " +
+                    "(_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " nombre VARCHAR)");
+        }
+        catch (SQLException e){
+            //Mensaje de error si no se ha ejecutado correctamente
+            Toast.makeText(contexto,""+e,Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //onUpgrade se ejecuta cuando se ACTUALIZA la versíon de la BD
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try {
+            //Eliminamos la tabla anterior (si existe)
+            db.execSQL("DROP TABLE IF EXISTS articulo");
+            //Llamamos a onCreate para que cree la tabla con las nuevas especificaciones
+            onCreate(db);
+        }
+        catch (SQLException e){
+            //Mensaje de error si no se ha ejecutado correctamente
+            Toast.makeText(contexto,""+e,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+}

04606cc93ef45c915272345e9780602f9233dd14
RadCres <raulduranc@hotmail.com>
2024-02-27 13:27:08 +0100

    UpdateLogo

--- EventosFuturos/app/src/main/java/com/example/eventosfuturos/MainActivity.java
+++ EventosFuturos/app/src/main/java/com/example/eventosfuturos/MainActivity.java
@@ -5,22 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText usuario,contrasena;
+    private EditText usuario,contrasena,email;
     private Button iniciarSesion;
     private TextView contrasenaOlvidada;
+    private Switch switchBoxSesion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         usuario = findViewById(R.id.editTextUsuario);
+        email = findViewById(R.id.EditTextEmail);
         contrasena = findViewById(R.id.editTextPassword);
         iniciarSesion = findViewById(R.id.buttonIniciarSesion);
-
+        switchBoxSesion = findViewById(R.id.switchInicio);
         contrasenaOlvidada = findViewById(R.id.textViewContraseñaOlvidada);
 
         iniciarSesion.setOnClickListener(v -> {
@@ -30,5 +34,16 @@ public class MainActivity extends AppCompatActivity {
             //Hacer los clausulas de guarda para pasar al activity de menus
         });
 
+
+    }
+    private boolean checkSwitch(){
+        if (switchBoxSesion.isChecked()){
+            switchBoxSesion.setText("Registrarse");
+            usuario.setVisibility(View.VISIBLE);
+            iniciarSesion.setText("Registrarse");
+        }
+        usuario.setVisibility(View.INVISIBLE);
+
+        return false;
     }
 }

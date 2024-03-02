2073cad7191132ef4b0441bad2935ec830c2be2e
Raúl <raulduranc@hotmail.com>
2024-03-02 13:01:12 +0100

    Nuevos Activities

--- EventosFuturos/app/src/main/java/com/example/eventosfuturos/MainActivityMenuInicioSesion.java
+++ EventosFuturos/app/src/main/java/com/example/eventosfuturos/MainActivityMenuInicioSesion.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivityMenuInicioSesion extends AppCompatActivity {
+import com.example.eventosfuturos.service.TaskCompleted;
+
+public class MainActivityMenuInicioSesion extends AppCompatActivity implements TaskCompleted<Boolean> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_inicio_sesion);
+    }
+
+    @Override
+    public void onTaskCompleted(Boolean aBoolean) {
+
     }
 }

df46a791270e095fd8fab84594571569704e0c03
Raúl <raulduranc@hotmail.com>
2024-03-04 17:35:21 +0100

    om evento

---
+++ EventosFuturos/app/src/main/java/com/example/eventosfuturos/OM/OM_Eventos.java
@@ -0,0 +1,9 @@
+package com.example.eventosfuturos.OM;
+
+import com.example.eventosfuturos.model.dto.Evento;
+
+import java.sql.Date;
+
+public class OM_Eventos {
+    Evento evento = new Evento(Date((null)))
+}
--- EventosFuturos/app/src/main/java/com/example/eventosfuturos/model/dto/Evento.java
+++ EventosFuturos/app/src/main/java/com/example/eventosfuturos/model/dto/Evento.java
@@ -7,4 +7,43 @@ public class Evento {
     private String titulo;
     private String descripcion;
     private String nombreGrupo;
+
+    public Evento(Date fecha, String titulo, String descripcion, String nombreGrupo) {
+        this.fecha = fecha;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.nombreGrupo = nombreGrupo;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombreGrupo() {
+        return nombreGrupo;
+    }
+
+    public void setNombreGrupo(String nombreGrupo) {
+        this.nombreGrupo = nombreGrupo;
+    }
 }
